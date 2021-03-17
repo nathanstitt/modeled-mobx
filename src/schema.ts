0f1574c6470a8e6ce05b9629feb96b7434f5655c
Nathan Stitt <nathan@stitt.org>
2021-03-17 15:15:19 -0500

    use weakmap instead of polluting object

--- src/schema.ts
+++ src/schema.ts
@@ -1,22 +1,21 @@
 import { ModelConstructor, ModelSchema as Schema, PropertyOptions } from './types';
 import { observable } from 'mobx'
 
+const Registry = new WeakMap()
+
 class ModelSchema<T, K extends keyof T> implements Schema<T, K> {
     properties = observable.map<K, PropertyOptions>({}, { deep: false })
 }
 
 export function getSchema<T>(ctor: ModelConstructor<T>): Schema<T, keyof T> | false {
-    return ctor.$mdmx || false
+    return Registry.get(ctor) || false
 }
 
 export function findOrCreateSchema<T>(ctor: ModelConstructor<T>): Schema<T, keyof T> {
-    if (ctor.$mdmx) { return ctor.$mdmx; }
-    const value = new ModelSchema()
-    Object.defineProperty(ctor, '$mdmx', {
-        enumerable: false,
-        writable: false,
-        configurable: true,
-        value,
-    });
-    return value
+    let schema = getSchema(ctor)
+    if (!schema) {
+        schema = new ModelSchema()
+        Registry.set(ctor, schema)
+    }
+    return schema
 }
